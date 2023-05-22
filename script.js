73645b87f03cf0aee1d55423dc6d4576dfdc351b
Iván Fráter <fraterivan03@gmail.com>
2023-05-22 15:05:13 +0200

    Version 1.0

---
+++ script.js
@@ -0,0 +1,81 @@
+dragElement(document.getElementById("window"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else { 
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+const tabs = document.querySelectorAll("menu[role=tablist]");
+
+for (let i = 0; i < tabs.length; i++) {
+  const tab = tabs[i];
+
+  const tabButtons = tab.querySelectorAll("menu[role=tablist] > button");
+
+  tabButtons.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      tabButtons.forEach((button) => {
+        if (
+          button.getAttribute("aria-controls") ===
+          e.target.getAttribute("aria-controls")
+        ) {
+          button.setAttribute("aria-selected", true);
+          openTab(e, tab);
+        } else {
+          button.setAttribute("aria-selected", false);
+        }
+      });
+    })
+  );
+}
+
+function openTab(event, tab) {
+  const articles = tab.parentNode.querySelectorAll('[role="tabpanel"]');
+  articles.forEach((p) => {
+    p.setAttribute("hidden", true);
+  });
+  const article = tab.parentNode.querySelector(
+    `[role="tabpanel"]#${event.target.getAttribute("aria-controls")}`
+  );
+  article.removeAttribute("hidden");
+}
+
+var today = new Date();
+var birthYear = 2003;
+var currentYear = today.getFullYear();
+document.getElementById("age").innerHTML = currentYear - birthYear;
+
+function closeButton() {
+  window.close();
+}
